ed976d66a2450f6f7caa6b7f2c38187a6c8400b0
zbansfield <zainab_bansfield@intuit.com>
2022-05-20 22:01:44 -0400

    final draft submission

--- js/script.js
+++ js/script.js
@@ -10,19 +10,63 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: 'The good deed and the bad deed are not the same. Return evil with good.',
+    source: 'Surah Fussilat, [41:34]'
+  },
+  {
+    quote: "I do not live in the prison of other people's opinion.",
+    source: 'Dr. Umar Abdullah Johnson',
+    year: '2016'
+  },
+  {
+    quote: "It's better to walk alone than with a crowd going in the wrong direction.",
+    source: 'Malcom X'
+  },
+  {
+    quote: "Racism is not getting worse, it's getting filmed.", 
+    source: 'Will Smith',
+    citation: 'The Late Show with Stephen Colbert',
+    year: 2016
+  },
+  {
+    quote: "If now isn't a good time for the truth, I don't see when we'll get to it.",
+    source: "Nikki Giovanni"
+  },
+  {
+    quote: 'It is not the eyes are blind, but the hearts.',
+    source: 'Surah Al-Haj, [22:46]'
+  },
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote(arr) {
+  const randomNum = Math.floor(Math.random() * arr.length);
+  return arr[randomNum];
+}
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote() {
+  const quote = getRandomQuote(quotes);
+  let html = `
+  <p class="quote">${quote.quote}</p>
+  <p class="source">${quote.source}
+    `;
+  if ( quote.citation ) {
+    html += `<span class="citation">${quote.citation}</span>`;
+  }
+  if ( quote.year ) {
+    html += `<span class="year">${quote.year}</span>`;
+  }    
+  html += `</p>`
+  document.getElementById('quote-box').innerHTML = html;
+}
 
 
 /***
